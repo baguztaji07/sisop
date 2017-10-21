c967f22d4d9840e88f73f1a418970621e9a80825
veaca <veaca13@yahoo.com>
2017-10-21 13:58:59 +0700

    jadi 1 thread dulu

--- no2.c
+++ no2.c
@@ -1,23 +1,42 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int x;
+int N,T;
 
-void print_prime(){
+void *print_prime(void *ptr){
 	int i=1;
-	for (int i=2;i<=x;i++){
+//	printf("a\n");
+	for (int i=2;i<=N;i++){
+//		printf ("cek angka %d\n", i);
 		int temp=0;
 		for (int j=2;j<=i;j++){
 			if (i%j==0){
 			temp++;
 			}
 		}
-	if (temp==1) printf ("%d ",i);
+	if (temp==1) printf ("prima : %i\n",i);
 	}
+	return NULL;
 }
 
 int main(){
-	printf ("input angka: ");
-	scanf ("%d", &x);
-	print_prime();
+	int err;
+	printf ("input N: ");
+	scanf ("%d", &N);
+	printf ("input T: ");
+	scanf ("%d", &T);
+//	N=10;
+	int jum=0;
+	pthread_t thread1;
+//	while (jum<T)
+//	{
+	err=pthread_create( &thread1, NULL, print_prime, NULL);
+//	print_prime();
+	if (err!=0){
+	printf ("cant create\n");}
+	else printf ("success\n");
+//	jum++;
+//	}
+	getchar();
+	pthread_join (thread1, NULL);
 }
