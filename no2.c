014251b0830e7b89ff7ec40b7124c2d2ae400778
veaca <veaca13@yahoo.com>
2017-10-22 16:13:49 +0700

    masi gabisa mutexnya

--- no2.c
+++ no2.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+pthread_mutex_t lock;
+
 typedef struct{
 	int bawah;
 	int atas;
 } batas;
 
 int N,T;
-//pthread_t tid[3];
 
 int cek(int N){
 	int i;
@@ -23,27 +24,16 @@ int cek(int N){
 void *print_prime(void *jum){
 	int count=0;
 	int baginya = N/T;
-//	pthread_t id=pthread_self();
 //	printf("a\n");
 	batas *bagian = jum;
-	//int angka= *((int*)jum);
-//	printf ("angka: %d\n", angka);
-//	if (pthread_equal(id,tid[count])){
+	pthread_mutex_lock (&lock);
+	printf ("batas bawah dan atas : %d, %d\n",bagian->bawah, bagian->atas);
 	for (int i=bagian->bawah;i<=bagian->atas;i++){
 //		printf ("cek bagian bawah & atas:%d , %d\n", bagian->bawah, bagian->atas);
 		int temp=0;
 		if (cek(i)) printf ("%d\n", i);
 	}
-	/*	for (int j=2;j<=i;j++){
-			if (i%j==0){
-			temp++;
-			}
-		}
-	if (temp==1) printf ("prima : %i\n",i);
-		}*/
-//	count++;
-//	angka++;
-//	}
+	pthread_mutex_unlock (&lock);
 	return 0;
 }
 
@@ -54,12 +44,12 @@ int main(){
 	printf ("input T: ");
 	scanf ("%d", &T);
 	batas bagian;
-//	N=10;
 	int jum=0;
 	int counter=1;
 	pthread_t tid[T+1];
+	pthread_mutex_init (&lock, NULL);
 	while (jum<T)
-	{
+	{	printf ("jumnya : %d\n", jum);
 		if (jum==T-1){
 			bagian.bawah=counter;
 			bagian.atas=N;}
@@ -67,16 +57,15 @@ int main(){
 			bagian.bawah=counter;
 			bagian.atas= counter + (N/T) - 1;
 			counter = counter +(N/T);}
-		printf ("counter nya: %d\n", counter);
+		printf ("start, end nya: %d, %d \n",bagian.bawah, bagian.atas);
 		err=pthread_create( &(tid[jum]), NULL, print_prime,(void*)&bagian);
-//	print_prime();
 	if (err!=0){
 	printf ("cant create\n");}
 	else printf ("success\n");
 	jum++;
-//	getchar();
+
 	}
-//	getchar();
+
 	for (int x=0;x<T;x++){
 	pthread_join (tid[x], NULL);
 	}
